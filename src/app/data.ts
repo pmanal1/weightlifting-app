51c89b650e5224c5579b5a19603206150d994a61
Paulo Manalo <paulomanalo792@gmail.com>
2024-07-30 15:06:58 -0400

    Extracts data to separate file and adds workout sets

---
+++ src/app/data.ts
@@ -0,0 +1,15 @@
+type Workout = {
+    name: string // name of the workout
+    sets: number // number of sets
+}
+
+export var pauloWorkout: Workout[] = [{
+    "name": "Bench Press",
+    "sets": 2
+}, {
+    "name": "Squat",
+    "sets": 3
+}, {
+    "name": "Deadlift",
+    "sets": 1
+}]
